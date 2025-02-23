aa6f7f093d56b729485e5def2bf7fa0f7985191b
Malini Piyumika <malini.softvilmedia@gmail.com>
2025-02-24 03:27:26 +0530

    fix: webhook errors

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,7 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { createOrUpdateUser, deleteUser } from "@/lib/actions/User";
+import { createOrUpdateUser, deleteUser } from "../../../lib/actions/User";
 
 export async function POST(req) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
