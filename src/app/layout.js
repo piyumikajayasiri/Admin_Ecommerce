67c3ed9b788a4c283cb410d90e7d1d686f18d7e9
Malini Piyumika <malini.softvilmedia@gmail.com>
2025-02-24 03:32:17 +0530

    fix: webhook errors 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,12 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
-import { ClerkLoaded, ClerkLoading, ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  ClerkProvider,
+  SignedIn,
+} from "@clerk/nextjs";
 import SideNavigationMenu from "../components/SideMenu";
 
 const geistSans = Geist({
@@ -34,12 +39,14 @@ export default function RootLayout({ children }) {
             </ClerkLoading>
             <ClerkLoaded>
               <Header />
-              <div className="flex flex-row">
-                <div className="w-[25%]">
-                  <SideNavigationMenu />
+              <SignedIn>
+                <div className="flex flex-row">
+                  <div className="w-[25%]">
+                    <SideNavigationMenu />
+                  </div>
+                  <div className="flex-grow-0 w-full">{children}</div>
                 </div>
-                <div className="flex-grow-0 w-full">{children}</div>
-              </div>
+              </SignedIn>
             </ClerkLoaded>
           </body>
         </html>
